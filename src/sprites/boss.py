90975f33ce702530195ff1cf578d1bf656681489
Eero Ranta <eeroranta111@gmail.com>
2022-04-25 19:09:37 +0300

    added boss movement

--- src/sprites/boss.py
+++ src/sprites/boss.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 
 class Boss(pygame.sprite.Sprite):
@@ -11,6 +12,9 @@ class Boss(pygame.sprite.Sprite):
         self.rect.y = 300
         self.color = 0
         self.hitpoints = 10
+        self.move_cooldown = 0
+        self.move_duration = 0
+        self.direction = 1
 
     def update(self):
         if self.color > 0:
@@ -18,6 +22,25 @@ class Boss(pygame.sprite.Sprite):
             self.image.fill((255, 28, 28))
         if self.color == 0:
             self.image.fill((0, 0, 0))
+        if self.move_cooldown > 100 and self.move_duration < 100:
+            if self.move_duration == 0:
+                random = randint(0, 1)
+                if random == 0:
+                    if self.rect.y < 700:
+                        self.direction = 1
+                    else:
+                        self.direction = -1
+                elif random == 1:
+                    if self.rect.y > 200:
+                        self.direction = -1
+                    else:
+                        self.direction = 1
+            self.rect.move_ip(0, self.direction)
+            self.move_duration += 1
+        if self.move_duration >= 100:
+            self.move_cooldown = 0
+            self.move_duration = 0
+        self.move_cooldown += 1
 
     def take_hit(self):
         self.hitpoints -= 1
